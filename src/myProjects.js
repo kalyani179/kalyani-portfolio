0da71cc23c67d4bbc778a6d4d8fa1a627f20fbae
Kalyani Dantuluri <dantulurikalyani999@gmail.com>
2023-07-15 23:40:25 +0530

    third-commit

--- src/myProjects.js
+++ src/myProjects.js
@@ -3,21 +3,21 @@ const MyProjects = [
         id:1,
         name:"MathTime",
         url:"https://kalyani179.github.io/Math-Time-1/",
-        logo:"./kalyani-portfolio/../../images/project-logos/yellow.png",
+        logo:"./kalyani-portfolio/images/project-logos/yellow.png",
         description:"HTML, CSS, BOOTSTRAP, JavaScript ( jQuery )"
     },
     {
         id:2,
         name:"ModernDictionary",
         url:"https://moderndictionary.onrender.com/",
-        logo:"./kalyani-portfolio/../../images/project-logos/encyclopedia.png",
+        logo:"./kalyani-portfolio/images/project-logos/encyclopedia.png",
         description:"HTML, CSS, BOOTSTRAP, Express.js, API, MongoDB"
     },
     {
         id:3,
         name:"Foosic",
         url:"",
-        logo:"./kalyani-portfolio/../../images/project-logos/foosic.png",
+        logo:"./kalyani-portfolio/images/project-logos/foosic.png",
         description:"HTML, CSS, BOOTSTRAP, React.js, Express.js, API, MongoDB"
     },
     {
